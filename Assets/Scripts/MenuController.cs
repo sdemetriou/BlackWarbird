2e4c6d1798cb10182beeee3debc74c765c615d80
4NN4GR4M <inbox@nellos.net>
2025-06-29 06:22:37 +0300

    Adding code for menu and ability system

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+  // Start is called before the first frame update
+
+  // The menu panel object
+  public GameObject menuPanel;
+
+  private bool MenuToggle;
+
+  void Start()
+  {
+    MenuToggle = true;
+    menuToggle();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.M))
+    {
+      menuToggle();
+      pauseGame();
+    }
+    else
+    {
+      menuToggle();
+      unpauseGame();
+    }
+  }
+
+  void menuToggle()
+  {
+    MenuToggle = !MenuToggle;
+    menuPanel.SetActive(MenuToggle);
+  }
+  void pauseGame()
+  {
+    Time.timeScale = 0;
+  }
+  void unpauseGame()
+  {
+    Time.timeScale = 1;
+  }
+}
---
+++ Assets/Scripts/WeaponAbilities.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAbilities : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public GameObject mainWeapon;
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  Dictionary<string, string> GunSourceAbility(string gunSourceAction)
+  {
+    Dictionary<string, string> output = new Dictionary<string, string>();
+    output.Add("GunSourceAction", gunSourceAction);
+    return output;
+  }
+
+  Dictionary<string, List<string>> GunTransAbility(Dictionary<string, string> source1, Dictionary<string, string> source2)
+  {
+    Dictionary<string, List<string>> output = new Dictionary<string, List<string>>();
+    List<string> combination = new List<string>() { source1["GunSourceAction"], source2["GunSourceAction"] };
+    output.Add("GunTransAction", combination);
+    return output;
+  }
+
+  void executor(Dictionary<string, List<string>> actionsToPerform)
+  {
+    List<string> actions = actionsToPerform["GunTransAction"];
+    foreach (string action in actions)
+    {
+      switch (action)
+      {
+        case "SHOOT_PROJ_PLASMA":
+          break;
+      }
+    }
+  }
+}
