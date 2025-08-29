c96138a1287078e886e543d53bfebfbad78cd7ec
Jason <123197994+JasonSlade@users.noreply.github.com>
2025-08-29 21:26:36 +0200

    General updates
    - Improved scoring system
    - Win condition + scene
    - Background music for game + menus

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // keep playing across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // prevent duplicates
+        }
+    }
+}
+
